53660cd4bd661bee004a691590f0357f0cea10df
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-01-02 21:46:56 +0900

    Time: 0 ms (100%), Space: 53.7 MB (72.62%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    let p = 0;
+    for (let i = 0 ; i < nums.length ; i++) {
+        if (nums[i] !== 0) {
+            nums[p] = nums[i];
+            p++;
+        }
+    }
+    
+    for (let i = p; i < nums.length ; i++) {
+        nums[i] = 0;
+    }
+
+    return nums;
+};
