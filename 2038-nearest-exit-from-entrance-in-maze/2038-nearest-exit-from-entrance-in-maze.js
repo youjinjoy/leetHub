9c541f4c17d31e3f3d833d2cf1096ead154f92ef
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-01-10 00:12:35 +0900

    Time: 284 ms (7.03%), Space: 62.7 MB (32.29%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.js
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.js
@@ -5,35 +5,30 @@
  */
 var nearestExit = function(maze, entrance) {
 
-    function bfs() {
-        const queue = [[...entrance, 0]];
-        const dx = [1, -1, 0, 0];
-        const dy = [0, 0, 1, -1];
+    const queue = [[...entrance, 0]];
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
 
-        const colLen = maze.length;
-        const rowLen = maze[0].length;
-        const visited = Array.from({length: colLen}, () => new Array(rowLen).fill(false));
-        visited[entrance[0]][entrance[1]] = true;
+    const colLen = maze.length;
+    const rowLen = maze[0].length;
+    const visited = Array.from({length: colLen}, () => new Array(rowLen).fill(false));
+    visited[entrance[0]][entrance[1]] = true;
 
-        while (queue.length) {
-            const [cx, cy, cDepth] = queue.shift();
-            
-            for (let i = 0 ; i < 4 ; i++) {
-                const [nx, ny, nDepth] = [cx+dx[i], cy+dy[i], cDepth + 1];
-                if (nx >= 0 && nx < colLen && ny >= 0 && ny < rowLen && !visited[nx][ny] && maze[nx][ny] !== '+') {
-                    console.log(nx,ny);
-                    if (nx === 0 || ny === 0 || nx === colLen-1 || ny === rowLen-1) {
-                        return nDepth;
-                    }
-                    queue.push([nx, ny, nDepth]);
-                    visited[nx][ny] = true;
+    while (queue.length) {
+        const [cx, cy, cDepth] = queue.shift();
+        
+        for (let i = 0 ; i < 4 ; i++) {
+            const [nx, ny, nDepth] = [cx+dx[i], cy+dy[i], cDepth + 1];
+            if (nx >= 0 && nx < colLen && ny >= 0 && ny < rowLen && !visited[nx][ny] && maze[nx][ny] !== '+') {
+                console.log(nx,ny);
+                if (nx === 0 || ny === 0 || nx === colLen-1 || ny === rowLen-1) {
+                    return nDepth;
                 }
+                queue.push([nx, ny, nDepth]);
+                visited[nx][ny] = true;
             }
         }
-
-        return -1;
     }
-    
 
-    return bfs();
-};
+    return -1;
+}
