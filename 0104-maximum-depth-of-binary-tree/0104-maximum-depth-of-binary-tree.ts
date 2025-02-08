e380550cb36937614c851840bf3d42040a63e0d3
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-02-08 13:10:03 +0900

    Time: 4 ms (10.53%), Space: 54.1 MB (49.33%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -13,17 +13,16 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-    if (root == null) {
-        return 0;
+    // 1차원
+    // DFS 재귀
+    function recursiveDFS(node: TreeNode | null, depth: number) {
+        if (!node) return depth;
+
+        return Math.max(recursiveDFS(node.left, depth + 1), recursiveDFS(node.right, depth + 1));
     }
 
-    return traverse(root, 0);
+    return recursiveDFS(root, 0);
+    // DFS 반복문
+    // BFS 재귀
+    // BFS 반복문
 };
-
-function traverse(node: TreeNode | null, depth: number):number {
-    if (node === null) {
-        return depth;
-    }
-    
-    return Math.max(traverse(node.left, depth+1), traverse(node.right, depth+1));
-}
