4216a7f2d8dfe886bfdc53fe0f52c7826fbf85d0
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-02-19 22:56:26 +0900

    Time: 5 ms (19.26%), Space: 58.7 MB (5.08%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,5 +1,21 @@
 function reverseWords(s: string): string {
-    const S = s.split(/\s+/);
-    S.reverse();
-    return S.join(' ').trim();
+    const result = [];
+    let word = '';
+    for (let element of s) {
+        if (element !== ' ') {
+            word += element;
+        }
+        else if (word.length) {
+            result.push(word);
+            word = '';
+        }
+    }
+
+    if (word.length) {
+        result.push(word);
+    }
+    
+    result.reverse();
+
+    return result.join(' ');
 };
