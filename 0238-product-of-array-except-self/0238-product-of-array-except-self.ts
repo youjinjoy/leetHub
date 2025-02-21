f2cb657eb770711faaf38c0f36b3df2825bc0221
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-02-21 13:47:22 +0900

    Time: 12 ms (38.25%), Space: 68.5 MB (25.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -1,36 +1,20 @@
 function productExceptSelf(nums: number[]): number[] {
-    // 0이 2개 이상 있으면 Output은 전부 0
-    // 0이 1개 있으면 Output은 0 제외 전부 0
-    // 0이 없으면 Output
+    const N = nums.length;
+    let leftProduct = 1;
+    let rightProduct = 1;
+    const result = Array(N).fill(1);
 
-    let productAll = 1;
-    let zeros = 0;
-    let zeroIndex= -1;
-    for (let i = 0 ; i < nums.length ; i++) {
-        if (zeros > 1) break;
-
-        if (nums[i] === 0) {
-            zeros += 1;
-            zeroIndex = i;
-        }
-        else {
-            productAll *= nums[i];
-        }
+    for (let i = 0 ; i < N ; i++) {
+        result[i] = leftProduct;
+        leftProduct *= nums[i];
     }
+    console.log("L",result);
 
-    if (zeros > 1) {
-        return Array(nums.length).fill(0);
+    for (let i = N-1 ; i >= 0 ; i--) {
+        result[i] *= rightProduct;
+        rightProduct *= nums[i];
     }
-    
-    if (zeros > 0) {
-        return nums.map((num,index) => {
-            if (index === zeroIndex) {
-                return productAll;
-            }
+    console.log("R",result);
 
-            return 0;
-        })
-    }
-
-    return nums.map((num) => productAll/num);
+    return result;
 };
