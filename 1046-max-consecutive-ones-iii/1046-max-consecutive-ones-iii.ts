71f14e26f1a76de7f68092353d26ae4825903d01
youjin <123347724+youjinjoy@users.noreply.github.com>
2025-03-19 22:21:01 +0900

    Time: 2 ms (84.72%), Space: 61 MB (45.64%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.ts
@@ -0,0 +1,24 @@
+function longestOnes(nums: number[], k: number): number {
+    let left = 0;
+    
+    let zeros = 0;
+
+    let maxOnes = 0;
+
+    for (let right = 0 ; right < nums.length ; right++) {
+        if (nums[right] === 0) {
+            zeros += 1;
+        }
+
+        while (zeros > k) {
+            if (nums[left] === 0) {
+                zeros -= 1;
+            }
+            left += 1;
+        }
+
+        maxOnes = Math.max(maxOnes, right - left + 1);
+    }
+    
+    return maxOnes;
+};
